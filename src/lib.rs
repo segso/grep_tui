7f07349ce6c5aa43377449cf05e2bdd93639f17b
SEG <segdev.mx@gmail.com>
2023-06-12 03:25:47 -0500

    feat: add a lib.rs with functions for set and restore the terminal, and run the app

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+use std::io;
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyModifiers},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    Terminal,
+};
+
+use app::App;
+
+pub mod app;
+pub mod component;
+
+pub fn setup_terminal<B: Backend>() -> Result<Terminal<B>, io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+
+    Terminal::new(backend)
+}
+
+pub fn restore_terminal<B: Backend>(mut terminal: Terminal<B>) -> Result<(), io::Error> {
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
+
+pub fn run_app<B: Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    let mut app = App::new();
+
+    loop {
+        terminal.draw(|f| app.draw(f))?;
+
+        let event = event::read()?;
+
+        let Event::Key(key) = event else {
+            continue;
+        };
+
+        if app.focused_index.is_none() {
+            if let KeyCode::Char('c') = key.code {
+                if key.modifiers.intersects(KeyModifiers::CONTROL) {
+                    return Ok(());
+                }
+            }
+        }
+
+        app.handle_key(key.code);
+    }
+}
